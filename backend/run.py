754c3f182fd6b2bdc6dd79198f4d4f50b6ec2ac8
vppatel4 <vppatel4@asu.edu>
2025-11-20 10:08:18 +0530

    fixed port

--- backend/run.py
+++ backend/run.py
@@ -6,5 +6,5 @@ app = create_app() # Calls the function defined in app/__init__.py
 
 if __name__ == '__main__':
     # Add your host and port as needed, debug=True for development
-    # Using port 5001 to avoid conflict with macOS AirPlay Receiver on port 5000
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    # Using port 5000 to avoid conflict with macOS AirPlay Receiver on port 5000
+    app.run(host='0.0.0.0', port=5000, debug=True)
